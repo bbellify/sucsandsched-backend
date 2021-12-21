86d58ebbc1627f0b8a3b92294d2aac0bd5a23a65
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-21 10:53:01 -0800

    endpoints stubbed

--- api/potlucks/potluck-router.js
+++ api/potlucks/potluck-router.js
@@ -11,6 +11,27 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
+router.post('/', (req, res, next) => {
+  res.json({ post: 'being built' })
+})
+
+router.get('/:id', (req, res, next) => {
+  res.json({ potluck: 'info coming' })
+})
+
+router.put('/:id', (req, res, next) => {
+  res.json({ update: 'being built' })
+})
+
+router.get(':id/guests', (req, res, next) => {
+  res.json({ guest_list: 'list being built' })
+})
+
+router.get(':id/items', (req, res, next) => {
+  res.json( { item_list: 'being built'})
+})
+
+
 
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(err.status || 500).json({
