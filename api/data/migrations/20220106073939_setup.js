8c59b29ddf7e18a454eefb264d75d1a9cb150acc
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-07 09:49:01 -0800

    setting up auth, migration, seeds

--- api/data/migrations/20220106073939_setup.js
+++ api/data/migrations/20220106073939_setup.js
@@ -6,10 +6,35 @@ exports.up = async (knex) => {
         table.integer('crunches')
         table.integer('squats')
       })
-      .createTable('table2', table => {
-        table.increments('id')
+      .createTable('users', table => {
+        table.increments('user_id')
+        table.string('username', 15).notNullable()
+        table.string('first_name', 15).notNullable()
+        table.string('password', 16).notNullable()
+        table.boolean('does_sucs').defaultTo(false)
+        table.string('role').defaultTo('user')
+
+      })
+      .createTable('races', table => {
+        table.increments('race_id')
+        table.string('past').defaultTo(false)
+        table.string('race_name').notNullable()
+        table.string('race_date').notNullable()
+        table.string('race_location').notNullable()
+        table.string('race_website').notNullable()
+      })
+      .createTable('users_races', table => {
+        table.increments('user_races_id')
+        table.integer('user_id')
+          .unSigned()
+          .notNullable()
+          .references('user_id')
+          .inTable('users')
+          .onUpdate('RESTRICT')
+          .onDelete('RESTRICT')
       })
       
+
   }
   
   exports.down = async (knex) => {
